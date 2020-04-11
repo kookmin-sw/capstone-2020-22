7fcd046ba75d7f52593883fe3946d04d478c0c2b
kooym5607 <starkoo06@kookmin.ac.kr>
2020-04-11 17:06:32 +0900

    네비게이션 실행 버튼 표시  및 구현

--- src/navigatAR/app/src/main/java/com/capstone/navigatAR/NavigationActivity.java
+++ src/navigatAR/app/src/main/java/com/capstone/navigatAR/NavigationActivity.java
@@ -1,13 +1,21 @@
 package com.capstone.navigatAR;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.mapbox.android.core.location.LocationEngine;
@@ -21,13 +29,13 @@ import com.mapbox.api.directions.v5.DirectionsCriteria;
 import com.mapbox.api.directions.v5.MapboxDirections;
 import com.mapbox.api.directions.v5.models.DirectionsResponse;
 import com.mapbox.api.directions.v5.models.DirectionsRoute;
-import com.mapbox.geojson.Feature;
-import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.LineString;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.location.LocationComponent;
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
@@ -37,14 +45,13 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
-import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
-import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
-import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions;
 import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
 import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
 
+import java.io.IOException;
 import java.lang.ref.WeakReference;
-import java.security.acl.Permission;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -73,6 +80,10 @@ public class NavigationActivity extends AppCompatActivity implements Permissions
     double destinationLng,destinationLat;
     public static double lat, lng;
     private Marker clickMarker;
+    private Button startButton;
+    EditText editText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -84,10 +95,86 @@ public class NavigationActivity extends AppCompatActivity implements Permissions
         mapView.onCreate(savedInstanceState);
         Log.e(TAG,"mapview onCreate 실행");
         mapView.getMapAsync(this);
+
+        startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startButton.setEnabled(false);
+
+                NavigationLauncherOptions options = NavigationLauncherOptions.builder()
+                        .directionsRoute(currentRoute)
+                        .build();
+                // Call this method with Context from within an Activity
+                NavigationLauncher.startNavigation(NavigationActivity.this, options);
+                //네비게이션 실행 (MainActivity에서)
+            }
+        });
+
+        Button myLoc_button = findViewById(R.id.myLoc_button); // 내 위치로 가는 버튼
+        myLoc_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                CameraPosition position = new CameraPosition.Builder()
+                        .target(new LatLng(lat, lng)) // Sets the new camera position
+                        .zoom(17) //  줌 정도 숫자가 클수록 더많이 줌함
+                        .bearing(0) // Rotate the camera , 카메라 방향(북쪽이 0) 북쪽부터 시계방향으로 측정
+                        .tilt(0) // Set the camera tilt , 각도
+                        .build(); // Creates a CameraPosition from the builder
+                //카메라 움직이기
+                mapboxMap.animateCamera(CameraUpdateFactory
+                        .newCameraPosition(position), 7000);
+                Toast.makeText(getApplicationContext(), String.format("            내위치 \n위도 : " + lat + "\n경도 : "+ lng), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+// onCreate 끝
+
+    @Override
+    public boolean onMapClick(@NonNull LatLng point){
+        Log.e(TAG,"onMapClick 실행");
+        Handler mHandler = new Handler();
+        if(clickMarker!=null) //1개의 마커만 표시.
+            mapboxMap.removeMarker(clickMarker);
+
+        clickMarker = mapboxMap.addMarker(new MarkerOptions().position(point));
+        destinationPos = Point.fromLngLat(point.getLongitude(), point.getLatitude());
+        myPos = Point.fromLngLat(lng,lat);
+
+        getRoute(myPos,destinationPos,1); // default로 도보 경로 표시
+        getRoute_navi(myPos,destinationPos,1);
+
+        mHandler.postDelayed(new Runnable(){ // start버튼 활성화. 1초의 딜레이를 두어 에러 나는거 발생.
+            public void run(){
+                startButton.setEnabled(true);
+            }
+        }, 1000);
+
+        return false;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 200){
+            if(resultCode == RESULT_OK && data != null){
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                editText.setText(result.get(0));
+
+                startButton.setEnabled(true);
+                getPointFromGeoCoder(editText.getText().toString());
+                Point origin = Point.fromLngLat(lng,lat);
+                Point destination = Point.fromLngLat(destinationLng, destinationLat);
+                getRoute(origin,destination,1);//폴리라인 그리기
+                getRoute_navi(origin,destination,1);
+            }
+        }
     }
 
     @Override
     public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+        Log.e(TAG, "onMapReady");
+
         NavigationActivity.this.mapboxMap = mapboxMap;
         mapboxMap.addOnMapClickListener(NavigationActivity.this);
         mapboxMap.setStyle(new Style.Builder().fromUri("mapbox://styles/kooym/ck82rmy5h28js1ioa295dzgc7"), new Style.OnStyleLoaded() {
@@ -98,20 +185,20 @@ public class NavigationActivity extends AppCompatActivity implements Permissions
         });
     }
 
-    @Override
-    public boolean onMapClick(@NonNull LatLng point){
-        Log.e(TAG,"onMapClick 실행");
-        if(clickMarker!=null) //1개의 마커만 표시.
-            mapboxMap.removeMarker(clickMarker);
-
-        clickMarker = mapboxMap.addMarker(new MarkerOptions().position(point));
-        destinationPos = Point.fromLngLat(point.getLongitude(), point.getLatitude());
-        myPos = Point.fromLngLat(lng,lat);
-
-        getRoute(myPos,destinationPos); //
-        getRoute_navi(myPos,destinationPos,1);
-        return false;
+    public void getPointFromGeoCoder(String destinationxy) {
+        Log.e(TAG,"지오코더 실행");
+        Geocoder geocoder = new Geocoder(this);
+        List<Address> listAddress = null;
+        try {
+            listAddress = geocoder.getFromLocationName(destinationxy, 1);
+            destinationLng = listAddress.get(0).getLongitude();
+            destinationLat = listAddress.get(0).getLatitude();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
 
     private void enableLocationComponent(@NonNull Style loadedMapStyle) {
 // Check if permissions are enabled and if not request
@@ -139,15 +226,7 @@ public class NavigationActivity extends AppCompatActivity implements Permissions
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
 
-    @Override
-    public void onExplanationNeeded(List<String> permissionsToExplain) {
-        Toast.makeText(this, "위치 권한 승인주세요.", Toast.LENGTH_LONG).show();
-    }
 
     @Override
     public void onPermissionResult(boolean granted) {
@@ -190,13 +269,14 @@ public class NavigationActivity extends AppCompatActivity implements Permissions
 //      mapboxMap.addPolyline(new PolylineOptions().add(points).color(Color.parseColor("#3bb2d0")).width(5));
     }
 
-    private void getRoute(Point origin, Point destination) {
+
+    private void getRoute(Point origin, Point destination,int profile) {
         Log.e(TAG,"getRoute 실행");
         client = MapboxDirections.builder()
                 .origin(origin)//출발지 위도 경도
                 .destination(destination)//도착지 위도 경도
                 .overview(DirectionsCriteria.OVERVIEW_FULL)//정보 받는정도 최대
-                .profile(DirectionsCriteria.PROFILE_WALKING)//길찾기 방법(도보,자전거,자동차)
+                .profile(profile==1?DirectionsCriteria.PROFILE_WALKING:DirectionsCriteria.PROFILE_CYCLING)//길찾기 방법(도보,자전거,자동차)
                 .accessToken(getString(R.string.mapbox_access_token))
                 .build();
 
@@ -230,6 +310,7 @@ public class NavigationActivity extends AppCompatActivity implements Permissions
                         "목적지 거리 : " +distants+ " km"), Toast.LENGTH_LONG).show();
                 // Draw the route on the map
                 drawRoute(currentRoute);
+
             }
             @Override
             public void onFailure(Call<DirectionsResponse> call, Throwable throwable) {
@@ -240,6 +321,8 @@ public class NavigationActivity extends AppCompatActivity implements Permissions
     }
 
     private void getRoute_navi (Point origin, Point destinaton, int profile) {
+        Log.e(TAG,"getRoute_navi 실행");
+
         NavigationRoute.builder(this).accessToken(Mapbox.getAccessToken())
                 .profile(profile==1?DirectionsCriteria.PROFILE_WALKING:DirectionsCriteria.PROFILE_CYCLING)//도보 길찾기
                 .origin(origin)//출발지
@@ -268,6 +351,40 @@ public class NavigationActivity extends AppCompatActivity implements Permissions
                 });
     }
 
+    public void showSearchItem(){  // 가는 방법 고르는 다이얼로그
+        final CharSequence[] oItems = {"도보", "자전거"};
+        AlertDialog.Builder oDialog = new AlertDialog.Builder(this,
+                android.R.style.Theme_DeviceDefault_Light_Dialog_Alert);
+        oDialog.setTitle("방법을 선택하세요")
+                .setItems(oItems, new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        getPointFromGeoCoder(editText.getText().toString());
+                        Point origin = Point.fromLngLat(lng,lat);
+                        Point destination = Point.fromLngLat(destinationLng, destinationLat);
+                        if (which == 0 ) {
+                            //도보 길찾기 진행
+                            getRoute(origin,destination,1);//예상 시간 및 위도 경도 출력
+                            getRoute_navi(origin,destination,1);//네비게이션 정보 저장
+                            startButton.setEnabled(true);
+                        }
+                        else if ( which == 1) {
+                            //자전거 길찾기 진행
+                            getRoute(origin,destination,2);//예상 시간 및 위도 경도 출력
+                            getRoute_navi(origin,destination,2);//네비게이션 정보 저장
+                            startButton.setEnabled(true);
+
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "오류 발생", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .setCancelable(false) //뒤로가기로 취소 막기
+                .show();
+    }
     @Override
     @SuppressWarnings({"MissingPermission"})
     public void onStart() {
@@ -346,5 +463,14 @@ public class NavigationActivity extends AppCompatActivity implements Permissions
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this, "위치 권한 승인주세요.", Toast.LENGTH_LONG).show();
     }
 }
