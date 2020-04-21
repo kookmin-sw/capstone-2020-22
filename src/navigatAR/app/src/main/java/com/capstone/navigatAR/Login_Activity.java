afeef1a409abb47316cee686b7e6a1ef18d16256
hyungu's <jhg3522@kookmin.ac.kr>
2020-04-21 22:48:17 +0900

    자동로그인 구현 가끔 강제종료됨..이유모름

--- src/navigatAR/app/src/main/java/com/capstone/navigatAR/Login_Activity.java
+++ src/navigatAR/app/src/main/java/com/capstone/navigatAR/Login_Activity.java
@@ -8,6 +8,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
@@ -24,43 +25,66 @@ import static com.mapbox.mapboxsdk.LibraryLoader.load;
 public class Login_Activity extends AppCompatActivity implements View.OnClickListener {
 
     Button Button_btn; // 코드를 전달하는 Button
-    TextInputEditText TextInputEditText_code; //코드 받는 EditText
+    EditText EditText_code; //코드 받는 EditText
     CheckBox CheckBox_auto_login;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
+    String Login_Code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextInputEditText_code = findViewById(R.id.TextInputEditText_code);
+        EditText_code = findViewById(R.id.EditText_code);
         Button_btn = findViewById(R.id.Button_btn);
         CheckBox_auto_login = findViewById(R.id.CheckBox_auto_login);
 
         sharedPreferences = getSharedPreferences("LoginData",0);
-        editor = sharedPreferences.edit();
+        Login_Code = sharedPreferences.getString("Login_Code",null);
 
         if (sharedPreferences.getBoolean("CheckBox_auto_login",false)){
-            TextInputEditText_code.setText(sharedPreferences.getString("CODE",""));
+            EditText_code.setText(sharedPreferences.getString("Login_Code",""));
             CheckBox_auto_login.setChecked(true);
         }
-        Button_btn.setOnClickListener(this);
+
+        if(Login_Code != null){
+            if(Login_Code.equals("AR001")){
+                Toast.makeText(Login_Activity.this,Login_Code+"사용 가능한 코드입니다.",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Login_Activity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }else if (Login_Code == null) {
+            Button_btn.setOnClickListener(this);
+        }
     }
+
     @Override
     public void onClick(View v){
         if(CheckBox_auto_login.isChecked()){
-            Toast.makeText(this, "로그인", Toast.LENGTH_SHORT).show();
-            String CODE = TextInputEditText_code.getText().toString();
-            editor.putString("CODE", CODE);
-            editor.putBoolean("CheckBox_auto_login", true);
-            editor.commit();
-            Intent intent = new Intent(Login_Activity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        }else {
+            if(EditText_code.getText().toString().equals("AR001")){
+                SharedPreferences sharedPreferences = getSharedPreferences("LoginData",0);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("Login_Code", EditText_code.getText().toString());
+                editor.putBoolean("CheckBox_auto_login", true);
+                editor.commit();
+                Intent intent = new Intent(Login_Activity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+        }else{
+                Toast.makeText(Login_Activity.this,Login_Code+"사용 불가능한 코드입니다.",Toast.LENGTH_SHORT).show();
+            }
+        }else if(CheckBox_auto_login.isChecked() != true){
             editor.clear();
             editor.commit();
+            if(EditText_code.getText().toString().equals("AR001")){
+                Intent intent = new Intent(Login_Activity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            }
         }
     }
-}
+
+
