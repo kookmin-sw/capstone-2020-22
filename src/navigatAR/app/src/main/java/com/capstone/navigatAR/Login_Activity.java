ddd707a809b3bbf1be429dea2cb8cbae97431138
hyungu's <jhg3522@kookmin.ac.kr>
2020-05-20 01:55:08 +0900

    logout 구현 미완성

--- src/navigatAR/app/src/main/java/com/capstone/navigatAR/Login_Activity.java
+++ src/navigatAR/app/src/main/java/com/capstone/navigatAR/Login_Activity.java
@@ -39,9 +39,9 @@ public class Login_Activity extends AppCompatActivity implements View.OnClickLis
         EditText_code = findViewById(R.id.EditText_code);
         Button_btn = findViewById(R.id.Button_btn);
         CheckBox_auto_login = findViewById(R.id.CheckBox_auto_login);
-
-        sharedPreferences = getSharedPreferences("LoginData",0);
+        sharedPreferences = getSharedPreferences("Login_Code",0);
         Login_Code = sharedPreferences.getString("Login_Code",null);
+
 
         if (sharedPreferences.getBoolean("CheckBox_auto_login",false)){
             EditText_code.setText(sharedPreferences.getString("Login_Code",""));
@@ -62,31 +62,42 @@ public class Login_Activity extends AppCompatActivity implements View.OnClickLis
 
     @Override
     public void onClick(View v){
-        if(CheckBox_auto_login.isChecked()){
+        sharedPreferences = getSharedPreferences("Login_Code",0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        if(CheckBox_auto_login.isChecked()) {
             if(EditText_code.getText().toString().equals("AR001")){
-                SharedPreferences sharedPreferences = getSharedPreferences("LoginData",0);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.putString("Login_Code", EditText_code.getText().toString());
                 editor.putBoolean("CheckBox_auto_login", true);
                 editor.commit();
+                Toast.makeText(Login_Activity.this,Login_Code+" 사용 가능한 코드입니다.",Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(Login_Activity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
-        }else{
-                Toast.makeText(Login_Activity.this,Login_Code+"사용 불가능한 코드입니다.",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(Login_Activity.this,EditText_code.getText().toString()+" 사용 불가능한 코드입니다.",Toast.LENGTH_SHORT).show();
             }
-        }else if(!CheckBox_auto_login.isChecked() ){
-            if(Login_Code !="") {
+        }
+        else if(!CheckBox_auto_login.isChecked() ){
+            if(Login_Code !=null) {
                 editor.clear();
                 editor.commit();
             }
             if(EditText_code.getText().toString().equals("AR001")){
+                Toast.makeText(Login_Activity.this,EditText_code.getText().toString()+" 사용 가능한 코드입니다.",Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(Login_Activity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
+            else{
+                if(Login_Code !=null) {
+                    Toast.makeText(Login_Activity.this, Login_Code + " 사용 불가능한 코드입니다.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(Login_Activity.this, EditText_code.getText().toString() + " 사용 불가능한 코드입니다.", Toast.LENGTH_SHORT).show();
+                }
             }
         }
     }
+}
 
 
--- src/navigatAR/app/src/main/java/com/capstone/navigatAR/MainActivity.java
+++ src/navigatAR/app/src/main/java/com/capstone/navigatAR/MainActivity.java
@@ -3,18 +3,23 @@ package com.capstone.navigatAR;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
 public class MainActivity extends AppCompatActivity {
-  private  Button main_btn;
+    private  Button main_btn;
+    private  Button logout_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         main_btn = findViewById(R.id.main_btn);
+        logout_btn = findViewById(R.id.logout_btn);
 
         main_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +28,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        logout_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login_Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 
 
