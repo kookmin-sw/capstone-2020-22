1cf39baeddf151df83bfc2bb6d908be332aaea0f
kooym5607 <starkoo06@kookmin.ac.kr>
2020-05-19 22:38:02 +0900

    AR글래스 액티비티 구현 및 시간,거리 실시간 표현

--- epsonapp/WikitudeDemo/app/src/main/java/com/example/wikitudedemo/MainActivity.java
+++ epsonapp/WikitudeDemo/app/src/main/java/com/example/wikitudedemo/MainActivity.java
@@ -1,42 +1,94 @@
 package com.example.wikitudedemo;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.wikitude.architect.ArchitectStartupConfiguration;
 import com.wikitude.architect.WearableArchitectView;
+import com.wikitude.common.camera.CameraSettings;
+import com.wikitude.common.startup.StartupConfiguration;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     private WearableArchitectView architectView;
+    private TextView timeText;
+    private TextView distanceText;
+    private double longitude,lagitude;
+    private Location location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setFullScreen();
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-        architectView = new WearableArchitectView(this);
+        this.architectView = (WearableArchitectView)findViewById(R.id.architectView);
+        loadArchitectView();
 
-        ArchitectStartupConfiguration configuration = new ArchitectStartupConfiguration();
-        configuration.setLicenseKey("z7tHao+2imWDz0SiiLi+vEQQQGQumzStTe6VeEvxiigoZivp5Wjawb9QwbGGyJFWZTdvrinZ1/+HlHq2N7yaieDRo98D1Ks1sLXlT1gw7hnvyx0d0MrPUXqmP9DtZE+KzsKIUjTwc4Ac+DmK3qEYrHRJyAg6CerMnQRGAExsnhVTYWx0ZWRfX9qTtGQ7/u4oBnscsUb9FSoIG97pOK1+4bZTloSUhybXy2WFbs5UgQY+zzSSp3+GZt+x5w97kT12MExK1QuzJvflEi1Afj3VOCOK3wA91KtPRPIIvkgJs3W+3WdOeSh2hGtvgRXGOOkjx9jpm89Z+hVQ6OtSoDswWNDFl8GUtSMTtAUucgtJT3karNcr5+l7PfCMaxLqy9fFXGalZnQuSAXzHvmmPcD1IBR7E5b2Pqkw2pXGThJvf8qSnBD7JHjAlwcfhb/dk681Lzu6o8ekwbBCePrepvWy1YbJLWHoA3f6WtqHKLMZS/SdowtFBXltp39kXSOnepmQ1jNpzEyNcZLgpaEBXFxzxoN+aE33048NpDXU2uERhz3bp8z2uu0vHzeQgZK9AGp9XUCq6Gblq9aUgbV3ZtlKTkr0oGOQXc0JI1Uq7wWmqb+AnDZJcOMub05YB7qR3O0QDrJPTX+k9tkDa/oICeQsDLF79yPPUfFvn9Q2Q0UBzYXQFkqjCkn6B0GeFy+LrJa6ELLZSVwhFvLyT46cyiqQ6vd+S1ajVbmfDPvfi6Ujs1nJPId/PAdkju6PcW3/1iptJLg1gvgQHQqMq+ifNMHsvwWZuCfsn2jSIcCBUekHvovwkMB+K0pMICTmgVLRPMRmgptijRuptF2b/9aBSYYYGIpyMdYyrZSS1izn33vLokk=");
+        timeText = (TextView)findViewById(R.id.timeText);
+        distanceText = (TextView)findViewById(R.id.distanceText);
 
-        architectView.onCreate(configuration);
-        setContentView(architectView);
+        DatabaseReference distance_db = database.getReference("distance");
+        DatabaseReference time_db = database.getReference("time"); // db key 값 지정
+        time_db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getValue(int.class) != null){
+                    int value = dataSnapshot.getValue(int.class);
+                    Log.w("FireBaseData", "getData : " + value);
+                    timeText.setText(String.valueOf(value));
+                }else{
+                    Toast.makeText(MainActivity.this, "데이터 없음", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w("FireBaseData", "loadPost:onCancelled", databaseError.toException());
+            }
+        });
+        distance_db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getValue(double.class) != null){
+                    double value = dataSnapshot.getValue(double.class);
+                    Log.w("FireBaseData", "getData : " + value);
+                    distanceText.setText(String.valueOf(value));
+                }else{
+                    Toast.makeText(MainActivity.this, "데이터 없음", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w("FireBaseData", "loadPost:onCancelled", databaseError.toException());
+            }
+        });
+
     }
 
     @Override
     protected void onPostCreate(final Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        architectView.onPostCreate();
 
-        try{
-            architectView.load("index.html");
-        }catch(IOException e){
-            e.printStackTrace();
-        }
     }
 
     @Override
@@ -55,5 +107,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         architectView.onDestroy();
+    }
+
+    private void setFullScreen(){
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN|
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE|
+                        View.SYSTEM_UI_FLAG_LOW_PROFILE|
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION|
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|
+                        View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+    private void loadArchitectView() {
+        final ArchitectStartupConfiguration configuration = new ArchitectStartupConfiguration();
+        configuration.setLicenseKey(getString(R.string.wikitude_license_key));
+        configuration.setCameraPosition(CameraSettings.CameraPosition.DEFAULT);
+        configuration.setCameraResolution(CameraSettings.CameraResolution.HD_1280x720);
+        configuration.setCameraFocusMode(CameraSettings.CameraFocusMode.CONTINUOUS);
+        this.architectView.onCreate(configuration);
+
+        this.architectView.onPostCreate();
+        try {
+            this.architectView.load("ArGeo/index.html");
+        }catch (IOException e){
+
+        }
+
+//        this.architectView.callJavascript("loadLogin()");
+//        this.architectView.callJavascript("loadWelcome()");
     }
 }
