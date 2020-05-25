f9efd4f82cceec64e2826cf74e4ea1bb81070dd4
hyungu's <jhg3522@kookmin.ac.kr>
2020-05-26 00:52:57 +0900

    main 하단바

---
+++ src/navigatAR/app/src/main/java/com/capstone/navigatAR/Fragment_help.java
@@ -0,0 +1,24 @@
+package com.capstone.navigatAR;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Fragment_help extends Fragment {
+    private FirebaseAuth firebaseAuth;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_help,container,false);
+        return view;
+    }
+}
